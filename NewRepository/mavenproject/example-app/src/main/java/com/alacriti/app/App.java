50c6138aad85699429a5a6352d236678ccd0159a
lakshmidurgac <lakshmidurga.chalamalasetti@alacriti.com>
2017-07-13 12:32:17 +0530

    this is example

---
+++ NewRepository/mavenproject/example-app/src/main/java/com/alacriti/app/App.java
@@ -0,0 +1,13 @@
+package com.alacriti.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
